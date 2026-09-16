0056db1585702618ecb92bdf4d5bb2488daec9a7
Kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2026-09-16 13:07:51 +0100

    feat(identity): add user service

---
+++ src/modules/identity/services/user.service.ts
@@ -0,0 +1,29 @@
+import type { UserRepository } from "../repositories/user.repository.js";
+
+
+type UserServiceDependencies = { 
+    userRepository: UserRepository
+ };
+
+
+export const createUserService = ({userRepository,}: UserServiceDependencies) => {
+
+  const getAllUsers = async () => {
+    return userRepository.findAll();
+  };
+
+  const getUserById = async (userId: string) => {
+    const user = await userRepository.findById(userId);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    return user;
+    
+  };
+  return {
+     getAllUsers, 
+     getUserById 
+    
+    };
+};
+export type UserService = ReturnType<typeof createUserService>;
